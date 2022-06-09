46c930678562ba43ee53023458b4d16604608ed8
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2022-06-09 19:23:53 +0300

    get broken

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export default function sequentialAsyncMap<T1, T2>(
   f: (item: T1, index: number, list: T1[]) => Promise<T2>,
 ): Promise<T2[]> {
   const a = Array.prototype.slice.call(collection, 0);
+  throw new Error('Test error');
   return a.reduce(
     async (accumulator, item, index, list) => [...(await accumulator), await f(item, index, list)],
     new Promise((resolve) => resolve([] as T2[])),
